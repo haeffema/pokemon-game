17a9cd946ee0e18cdac255f8b2b1f328ce88c852
haeffema <haeffema@gmail.com>
2025-03-31 12:14:19 +0000

    initial commit

---
+++ .idx/dev.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  env = {};
+  idx = {
+    extensions = [ "whizkydee.material-palenight-theme" "christian-kohler.npm-intellisense" "christian-kohler.path-intellisense" "dbaeumer.vscode-eslint" "esbenp.prettier-vscode" "rvest.vs-code-prettier-eslint"];
+
+    previews = {
+      enable = true;
+      previews = {};
+    };
+
+    workspace = {
+      onCreate = {
+        npm-install = "npm install";
+      };
+      onStart = {};
+    };
+  };
+}
---
+++ src/bot.js
@@ -0,0 +1,7 @@
+console.log('test');
+
+console.log('hallo weld');
+console.log('helo froinde');
+console.log('does sis worg');
+console.log('tet');
+console.log('das ist awseome');
