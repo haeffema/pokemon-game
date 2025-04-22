0c07877c620760206808cf8ba3a5036f0792e267
Jan <jan.stoerk@gmail.com>
2025-04-22 10:01:08 +0200

    items gefiltert

--- src/bot.js
+++ src/bot.js
@@ -76,3 +76,46 @@ import showdown from 'pokemon-showdown';
 const { Dex } = showdown;
 
 const pokedex = Dex.forGen(7);
+
+import { readFile } from 'fs/promises';
+
+async function filterPokemonByType(type, forbiddenTiers, number) {
+  try {
+    const data = await readFile('src/pokemon/data.json', 'utf8');
+    const pokemonObj = JSON.parse(data);
+
+    const allPokemon = Object.values(pokemonObj);
+   // Filtern mit optionalem Typ
+   const filtered = allPokemon.filter(p =>
+    (!type || p.types.includes(type)) &&
+    !forbiddenTiers.includes(p.tier)
+);
+
+  // Mischen des Arrays mit dem Fisher-Yates-Algorithmus
+  for (let i = filtered.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [filtered[i], filtered[j]] = [filtered[j], filtered[i]];
+}
+
+// Rückgabe der gewünschten Anzahl von Pokémon
+return filtered.slice(0, number);
+
+  } catch (err) {
+    console.error('Fehler beim Laden oder Verarbeiten der Datei:', err.message);
+    return [];
+  }
+}
+
+const Pokemon = await filterPokemonByType('Fire', ["Uber","OU","UUBL","UU","RUBL"], 20);
+console.log(`Gefundene Pokémon mit Typ: ${Pokemon.length}`);
+Pokemon.forEach(p => console.log(p.name));
+
+function calculateLoot() {
+  const minGold = 100;
+  const maxGold = 200;
+  const loot = Math.floor(Math.random() * (maxGold - minGold + 1)) + minGold;
+  return loot;
+}
+
+const gold = calculateLoot();
+console.log(`Du hast ${gold} Gold gefunden!`);
