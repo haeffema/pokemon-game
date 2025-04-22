c9bc69fb6bc999241976ca51f048305c1a1e2732
haeffema <haeffema@gmail.com>
2025-04-22 22:12:15 +0000

    somethings broken but very close to finish

--- src/pokemon/battle.js
+++ src/pokemon/battle.js
@@ -1,17 +1,79 @@
 import showdown from 'pokemon-showdown';
 const { Battle, Teams, Dex } = showdown;
+import { calculate, Generations, Pokemon, Move, Field } from '@smogon/calc';
 
 const trainerID = 'p1';
 const botID = 'p2';
 
-async function getTrainerMove(moves) {
-  console.log(moves);
+function formatForCalc(pokemon) {
+  const set = pokemon.set;
+  const evs = set?.evs || { hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0 };
+  const ivs = set?.ivs || {
+    hp: 31,
+    atk: 31,
+    def: 31,
+    spa: 31,
+    spd: 31,
+    spe: 31,
+  };
+  const nature = set?.nature || 'Serious';
+
+  return new Pokemon(pokemon.battle.gen, set.name, {
+    level: pokemon.level,
+    ability: pokemon.ability,
+    item: set?.item,
+    nature: nature,
+    evs: evs,
+    ivs: ivs,
+    boosts: pokemon.boosts,
+    gender:
+      pokemon.gender === 'M' ? 'M' : pokemon.gender === 'F' ? 'F' : undefined,
+    hp: pokemon.hp,
+    maxhp: pokemon.maxhp,
+  });
+}
+
+async function findBestMove(attackerShowdown, defenderShowdown) {
+  const gen = Generations.get(attackerShowdown.battle.gen);
+  const attacker = formatForCalc(attackerShowdown);
+  const defender = formatForCalc(defenderShowdown);
+  const moves = attackerShowdown.set.moves;
+  let bestMoveName = null;
+  let maxDamage = -1;
+  for (const moveName of moves) {
+    const move = new Move(gen, moveName);
+    const field = new Field({
+      weather: attackerShowdown.battle.field.weather || undefined,
+      terrain: attackerShowdown.battle.field.terrain || undefined,
+      defenderSide: {
+        reflect: defenderShowdown.volatiles['reflect']?.active || false,
+        lightScreen: defenderShowdown.volatiles['lightscreen']?.active || false,
+        auroraVeil: defenderShowdown.volatiles['auroraveil']?.active || false,
+      },
+      attackerSide: {
+        helpingHand: attackerShowdown.volatiles['helpinghand']?.active || false,
+      },
+    });
+    const result = calculate(gen, attacker, defender, move, field);
+    const possibleDamage = result.damage;
+    if (possibleDamage && possibleDamage.length > 0) {
+      const currentMaxDamage = Math.max(...possibleDamage);
+      if (currentMaxDamage > maxDamage) {
+        maxDamage = currentMaxDamage;
+        bestMoveName = moveName;
+      }
+    }
+  }
+  console.log(bestMoveName);
+}
+
+async function getTrainerMove(moves, ownPokemon, enemyPokemon) {
   return 1;
 }
 
-async function getBotMove(moves) {
-  console.log(moves);
-  return 1;
+async function getBotMove(moves, ownPokemon, enemyPokemon) {
+  await findBestMove(ownPokemon, enemyPokemon);
+  return 0;
 }
 
 function setupBattle(playerTeam, botTeam) {
@@ -31,12 +93,16 @@ function setupBattle(playerTeam, botTeam) {
 
 function displayBattleState(battle) {
   console.log('\n--- Turn ' + battle.turn + ' ---');
-  const p1 = battle.p1.active[0];
-  const p2 = battle.p2.active[0];
+  const trainerPokemon = battle.p1.active[0];
+  const wildPokemon = battle.p2.active[0];
 
   console.log('\n=== Battle State ===');
-  console.log(`Your ${p1.name} (HP: ${p1.hp}/${p1.maxhp})`);
-  console.log(`Wild ${p2.name} (HP: ${p2.hp}/${p2.maxhp})`);
+  console.log(
+    `Your ${trainerPokemon.name} (HP: ${trainerPokemon.hp}/${trainerPokemon.maxhp})`
+  );
+  console.log(
+    `Wild ${wildPokemon.name} (HP: ${wildPokemon.hp}/${wildPokemon.maxhp})`
+  );
   console.log('---');
 
   const lastLogEntry = battle.log[battle.log.length - 1];
@@ -78,10 +144,23 @@ function getAvailableMovesWithDescription(battle, playerId) {
   return [];
 }
 
-async function nextMove(battle, playerID, getChoice) {
+async function nextMove(battle, playerID) {
   const trainerMoves = getAvailableMovesWithDescription(battle, playerID);
   if (trainerMoves.length > 0 && !battle.ended) {
-    const choice = await getChoice(trainerMoves);
+    let choice = 0;
+    if (playerID == trainerID) {
+      choice = await getTrainerMove(
+        trainerMoves,
+        battle.p1.active[0],
+        battle.p2.active[0]
+      );
+    } else {
+      choice = await getBotMove(
+        trainerMoves,
+        battle.p2.active[0],
+        battle.p1.active[0]
+      );
+    }
     battle.choose(playerID, `move ${choice}`);
   }
 }
@@ -91,8 +170,8 @@ async function fightBotPokemon(playerTeam, botTeam) {
 
   while (!battle.ended) {
     displayBattleState(battle);
-    await nextMove(battle, trainerID, getTrainerMove);
-    await nextMove(battle, botID, getBotMove);
+    await nextMove(battle, trainerID);
+    await nextMove(battle, botID);
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
 
@@ -102,25 +181,25 @@ async function fightBotPokemon(playerTeam, botTeam) {
 }
 
 const trainerTest = `
-    Charizard @ Charizardite X
-    Ability: Blaze
-    EVs: 252 Atk / 252 Spe
-    Adamant Nature
-    - Flamethrower
-    - Roost
-    - Dragon Claw
-    - Earthquake
+Clawitzer @ Choice Specs
+Ability: Mega Launcher
+EVs: 4 Def / 252 SpA / 252 Spe
+Modest Nature
+- Water Pulse
+- Ice Beam
+- Dark Pulse
+- Aura Sphere
     `;
 
 const botTest = `
-    Charizard @ Charizardite X
-    Ability: Blaze
-    EVs: 252 Atk / 252 Spe
-    Adamant Nature
-    - Flamethrower
-    - Roost
-    - Dragon Claw
-    - Earthquake
+Darkrai @ Life Orb
+Ability: Bad Dreams
+EVs: 4 HP / 252 SpA / 252 Spe
+Timid Nature
+- Hypnosis
+- Nasty Plot
+- Dark Pulse
+- Thunder
     `;
 
 fightBotPokemon(trainerTest, botTest);
