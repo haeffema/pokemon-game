3b97611ee726ec8a5a0d3644e55ad87cd5c8d9b8
Haeffele, Maximilian <haeffema@gmail.com>
2025-04-30 18:53:20 +0200

    added empty function to pokemon.js

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -26,3 +26,5 @@ export function convertSetToPokepaste(set, name) {
   }
   return `${name} @ ${set.item}\nAbility: ${set.ability}\n${evs}\n${set.nature} Nature${moveStr}`;
 }
+
+export function getRandomSet(pokemon) {}
