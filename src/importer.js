5aeac98f97afed092c01d12ac06d973de1b24eaf
Haeffele, Maximilian <haeffema@gmail.com>
2025-05-06 10:20:26 +0200

    push to show for jan

--- src/importer.js
+++ src/importer.js
@@ -4,43 +4,100 @@ import fs from 'fs/promises';
 const newData = {};
 const failedKeys = [];
 
+const customPreEvos = {
+  'raticate-alola': 'rattata-alola',
+  'raichu-alola': 'pichu',
+  'sandslash-alola': 'sandshrew-alola',
+  'ninetales-alola': 'vulpix-alola',
+  'dugtrio-alola': 'diglett-alola',
+  'persian-alola': 'meowth-alola',
+  'golem-alola': 'geodude-alola',
+  'muk-alola': 'grimer-alola',
+  'exeggutor-alola': 'exeggcute',
+  'marowak-alola': 'cubone',
+  'rotom-mow': 'rotom-mow',
+  'rotom-fan': 'rotom-fan',
+  'rotom-frost': 'rotom-frost',
+  'rotom-heat': 'rotom-heat',
+  'rotom-wash': 'rotom-wash',
+  'mr. mime': 59,
+  basculin: 550,
+  'meowstic-f': 'meowstic',
+  gourgeist: 711,
+  oricorio: 741,
+  'oricorio-pom-pom': 741,
+  "oricorio-pa'u": 741,
+  'oricorio-sensu': 741,
+  'lycanroc-midnight': 745,
+  'lycanroc-dusk': 745,
+  wishiwashi: 746,
+  mimikyu: 778,
+};
+
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 for (const [key, value] of Object.entries(originalData)) {
   const oldData = value;
+  oldData['moves'] = {};
+
+  let response;
 
   try {
     console.log(`Processing ${key}...`);
 
-    let response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon-species/${value.id}`
-    );
-    if (!response.ok) throw new Error(`Species not found for ${key}`);
-    let data = await response.json();
+    let preEvo = customPreEvos[key];
 
-    await delay(200); // Delay between requests
+    if (!preEvo) {
+      response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${value.id}`
+      );
+      if (!response.ok) throw new Error(`Species not found for ${key}`);
+      preEvo = await response.json();
+      await delay(200);
+    }
 
-    response = await fetch(data.evolution_chain.url);
-    if (!response.ok) throw new Error(`Evolution chain not found for ${key}`);
-    data = await response.json();
+    let url;
+
+    if (!preEvo.evolution_chain) {
+      url = `https://pokeapi.co/api/v2/pokemon/${preEvo}`;
+    } else {
+      response = await fetch(preEvo.evolution_chain.url);
+      if (!response.ok) throw new Error(`Evolution chain not found for ${key}`);
+      let preEvoData = await response.json();
+      await delay(200);
+      url = `https://pokeapi.co/api/v2/pokemon/${preEvoData.chain.species.name}`;
+    }
+
+    response = await fetch(url);
+    if (!response.ok) throw new Error(`Base species data not found for ${url}`);
+    const baseSpecies = await response.json();
 
     await delay(200);
 
-    const baseSpecies = data.chain.species.name;
-    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${baseSpecies}`);
-    if (!response.ok)
-      throw new Error(`Base species data not found for ${baseSpecies}`);
-    data = await response.json();
+    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${key}`);
+    if (!response.ok) throw new Error(`Original data not found for ${key}`);
+    const originalData = await response.json();
 
-    for (const move of data.moves) {
-      const version = move.version_group_details.find(
+    for (const move of originalData.moves) {
+      const type = move.version_group_details.find(
+        (item) => item.version_group.name === 'ultra-sun-ultra-moon'
+      );
+      if (type) {
+        oldData['moves'][move.move.name] = {
+          name: move.move.name,
+          type: type.move_learn_method.name,
+        };
+      }
+    }
+    for (const move of baseSpecies.moves) {
+      const type = move.version_group_details.find(
         (item) =>
           item.version_group.name === 'ultra-sun-ultra-moon' &&
           item.move_learn_method.name === 'egg'
       );
-      if (version) {
+      if (type) {
         if (!Object.keys(oldData.moves).includes(move.move.name)) {
           oldData.moves[move.move.name] = {
             name: move.move.name,
@@ -49,7 +106,6 @@ for (const [key, value] of Object.entries(originalData)) {
         }
       }
     }
-
     newData[key] = oldData;
   } catch (err) {
     console.warn(`Skipping ${key}: ${err.message}`);
@@ -57,10 +113,9 @@ for (const [key, value] of Object.entries(originalData)) {
     continue;
   }
 
-  await delay(200); // Delay between each main loop iteration
+  await delay(200);
 }
 
-// Write updated data and failed keys
 await fs.writeFile('test.json', JSON.stringify(newData, null, 2));
 await fs.writeFile('failed_keys.json', JSON.stringify(failedKeys, null, 2));
 
